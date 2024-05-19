6cd4d5c45476197ee0b836656fd733edc08ee7ba
LarinNikita <larin_nikita_m@mail.ru>
2024-05-19 17:03:37 +0700

    initial setup project. Add shadcn vue. Setup and configurate prisma lucia

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+const user = useUser()
+
+async function logout() {
+    await $fetch('/api/logout', {
+        method: 'POST',
+    })
+    await navigateTo('/login')
+}
+
+definePageMeta({
+    middleware: 'protected',
+})
+</script>
+
+<template>
+    <div>
+        <pre
+            >{{ user }}
+        </pre>
+        <form @submit.prevent="logout">
+            <Button>Sign out</Button>
+        </form>
+    </div>
+</template>
+
+<style scoped></style>
